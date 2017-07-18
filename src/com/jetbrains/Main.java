acbff6085ab08b5a1f08d74f56d4985bf5196655
Katie <katie.Kourtakis@gmail.com>
2017-07-17 20:57:23 -0400

    added loop

--- src/com/jetbrains/Main.java
+++ src/com/jetbrains/Main.java
@@ -13,42 +13,51 @@ public class Main {
         int vowelsLength = vowels.length();
         int count = 0;
         int totalCount = 0;
+        String answer = "yes";
 
         Scanner scnr = new Scanner(System.in);
 
         System.out.println("Welcome to the Pig Latin Translator!");
 
-        System.out.print("Please enter a word: ");
-        userWord = scnr.next();
+        do {
 
-        int wordLength = lowerCaseWord(userWord).length();
+            totalCount = 0;
+            count = 0;
 
-        for (i = 0; i < vowelsLength; i++) {
-            char vowelLetter = vowels.charAt(i);
-            char wordLetter = lowerCaseWord(userWord).charAt(0);
-            if (vowelLetter == wordLetter) {
-                System.out.println("Translation: " + lowerCaseWord(userWord) + "way");
-                break;
+            System.out.print("Please enter a word: ");
+            userWord = scnr.next();
+
+            int wordLength = lowerCaseWord(userWord).length();
+
+            for (i = 0; i < vowelsLength; i++) {
+                char vowelLetter = vowels.charAt(i);
+                char wordLetter = lowerCaseWord(userWord).charAt(0);
+                if (vowelLetter == wordLetter) {
+                    System.out.println("Translation: " + lowerCaseWord(userWord) + "way");
+                    break;
+                } else {
+                    totalCount = 5;
+                }
             }
-            else {
-                totalCount = 5;
-            }
-        }
             for (i = 0; i < wordLength; i++) {
                 char wordConsLetter = lowerCaseWord(userWord).charAt(i);
                 for (s = 0; (s < vowelsLength) && (totalCount == 5); s++) {
                     char vowelLetter = vowels.charAt(s);
                     if (wordConsLetter != vowelLetter) {
                         count++;
-                    }
-                    else {
+                    } else {
                         totalCount = (count / 5);
                         System.out.println("Translation: " + (lowerCaseWord(userWord).substring(totalCount, wordLength)) +
                                 (lowerCaseWord(userWord).substring(0, totalCount)) + "ay");
+                        break;
                     }
                 }
             }
-        }
+            System.out.println("Would you like to translate another word? ");
+            answer = scnr.next();
+            scnr.nextLine();
+        } while (answer.equals("yes"));
+    }
 
 
     public static String lowerCaseWord(String userWord) {
